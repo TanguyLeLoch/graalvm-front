71eda8e89f301ea1514907bfefed192ada4b9593
Tanguy Le Loch <lelochtanguy@gmail.com>
2023-06-27 15:18:52 +0700

    landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./auth/components/login/login.component";
+import {LandingPageComponent} from "./landing-page/components/landing-page/landing-page.component";
 
 const routes: Routes = [
   {path: 'auth/login', component: LoginComponent},
-  {path: '', component: LoginComponent},
+  {path: '', component: LandingPageComponent},
 ];
 
 @NgModule({
